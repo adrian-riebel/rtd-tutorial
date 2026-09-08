d3a4ccde37d4a5131be13cbd65554089e788181e
adrian-riebel <111371360+adrian-riebel@users.noreply.github.com>
2026-09-08 10:27:00 -0300

    Create __init__.py

---
+++ sercpy/thermal/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 31 15:33:07 2026
+
+@author: adria
+"""
+
+from . import core
+from . import config
+from . import demand_profile
+from . import meteo_profile
